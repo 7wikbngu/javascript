c039782f6230e7911a2feab5344a872837b51843
Sathwik <sathwikdec@gmail.com>
2018-07-24 10:37:25 -0400

    initial commit

---
+++ Calculator/calc.js
@@ -0,0 +1,35 @@
+
+function operationCalc(){
+    
+    var input1 = Number(document.getElementById("input-1").value);
+    var input2 = Number(document.getElementById("input-2").value);
+    var operator = Number(document.getElementById("menu").value);
+
+    var result = 0;
+
+    switch (operator) {
+        case 1: 
+            result = input1 + input2;
+            document.getElementById("calc").innerHTML = result;
+            break;
+        case 2:
+            result = input1 - input2;
+            document.getElementById("calc").innerHTML = result;
+            break;
+        case 3:
+            result = input1 * input2;
+            document.getElementById("calc").innerHTML = result;
+            break;
+        case 4:
+            result = input1 / input2;
+            document.getElementById("calc").innerHTML = result.toFixed(2);
+            break;
+        case 5:
+            result = input1 % input2;
+            document.getElementById("calc").innerHTML = result;
+            break;
+        default:
+            document.getElementById("calc").innerHTML = "Error Occured";
+    }    
+
+}
